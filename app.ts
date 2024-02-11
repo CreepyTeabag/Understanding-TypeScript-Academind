b2af575b1497954dc714cd14f948a72cf3474d23
CreepyTeabag <sovunja1712@gmail.com>
2024-02-11 19:08:21 +0300

    Watched until 1:36:30

--- app.ts
+++ app.ts
@@ -13,32 +13,38 @@
 //   hobbies: ["Sports", "Cooking"],
 //   role: [2, "author"],
 // };
+
 // const ADMIN = 0;
 // const READ_ONLY = 1;
 // const AUTHOR = 2;
-var Role;
-(function (Role) {
-    Role[Role["ADMIN"] = 0] = "ADMIN";
-    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
-    Role[Role["AUTHOR"] = 2] = "AUTHOR";
-})(Role || (Role = {}));
-var person = {
-    name: "Sophie",
-    age: 26,
-    hobbies: ["Sports", "Cooking"],
-    role: Role.ADMIN,
+
+enum Role {
+  ADMIN = 5,
+  READ_ONLY,
+  AUTHOR,
+}
+
+const person = {
+  name: "Sophie",
+  age: 26,
+  hobbies: ["Sports", "Cooking"],
+  role: Role.ADMIN,
 };
+
 // person.role.push("admin");
 // person.role[1] = 10;
 // person.role = [10, 'admin', 'user'];
-var favoriteActivities;
+
+let favoriteActivities: string[];
 favoriteActivities = ["Sports"];
+
 console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase);
-    // console.log(hobby.map()); // ERROR
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase);
+  // console.log(hobby.map()); // ERROR
 }
+
 if (person.role === Role.ADMIN) {
-    console.log("is admin");
+  console.log("is admin");
 }
