35c0ce6812355c700918e630f53811783f3d5974
Koniukhova Sofia <sovunja1712@gmail.com>
2024-02-26 10:58:40 +0300

    Watched until 2:21:08

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-function add(n1, n2) {
-    return n1 + n2;
+var userInput;
+var userName;
+userInput = 5;
+userInput = "Max";
+// userName = userInput; // error
+if (typeof userInput === "string") {
+    userName = userInput;
 }
-function printResult(num) {
-    console.log("Result: ", num);
+function generateError(message, code) {
+    throw { message: message, errorCode: code };
+    // while (true) {}
 }
-printResult(add(5, 12));
-// let combineValues: Function;
-var combineValues;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-console.log(combineValues(8, 8));
+generateError("An error occurred!", 500);
