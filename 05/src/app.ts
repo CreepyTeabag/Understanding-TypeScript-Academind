b007602525494e772871d562816a934c648c0d7c
Koniukhova Sofia <sovunja1712@gmail.com>
2025-03-20 15:51:53 +0300

    Watched until 05.10. Overriding Properties & The protected Modifier

--- 05/src/app.ts
+++ 05/src/app.ts
@@ -1,7 +1,7 @@
 class Department {
   // private id: string;
   // private name: string; // field of a class
-  private employees: string[] = [];
+  protected employees: string[] = [];
 
   constructor(private readonly id: string, public name: string) {
     // this.id = id;
@@ -21,19 +21,61 @@ class Department {
   }
 }
 
-const accounting = new Department("d1", "Accounting"); // instantiation of a class
+class ITDepartment extends Department {
+  admins: string[];
+  constructor(id: string, admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "accounting");
+  }
+
+  addEmployee(employee: string): void {
+    if (employee === "Max") {
+      return;
+    }
+    this.employees.push(employee);
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  getReports() {
+    console.log(this.reports);
+  }
+}
+
+// const accounting = new Department("d1", "Accounting"); // instantiation of a class
+const it = new ITDepartment("d1", ["Sophie"]); // instantiation of a class
+
+it.addEmployee("Sophie");
+it.addEmployee("Max");
+
+// it.employees[2] = "Manu";
+
+it.describe();
+it.name = "NEW NAME";
+
+it.printEmployeeInformation();
+
+// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
+// accountingCopy.describe();
+
+console.log(it);
+
+const accounting = new AccountingDepartment("d2", []);
+
+accounting.addReport("Something went wrong...");
+accounting.addReport("Something went wrong again...");
 
 accounting.addEmployee("Sophie");
 accounting.addEmployee("Max");
 
-// accounting.employees[2] = "Manu";
+accounting.getReports();
 
 console.log(accounting);
-
-accounting.describe();
-accounting.name = "NEW NAME";
-
-accounting.printEmployeeInformation();
-
-// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
-// accountingCopy.describe();
