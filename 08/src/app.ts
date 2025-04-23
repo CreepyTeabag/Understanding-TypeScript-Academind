039852a93c2d03834bb7dcfe99798d2dea6ec6b9
CreepyTeabag <sovunja1712@gmail.com>
2025-04-23 21:52:51 +0300

    Watched until 08.11. Example Creating an Autobind Decorator

--- 08/src/app.ts
+++ 08/src/app.ts
@@ -1,39 +1,46 @@
-// function Logger(logString: string) {
-//   console.log("LOGGER FACTORY");
-//   return function (constructor: Function) {
-//     console.log(logString);
-//     console.log(constructor);
-//   };
-// }
+function Logger(logString: string) {
+  console.log("LOGGER FACTORY");
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
+}
 
-// function WithTemplate(template: string, hookId: string) {
-//   console.log("TEMPLATE FACTORY");
-//   return function (constructor: any) {
-//     console.log("Rendering template");
-//     const hookEl = document.getElementById(hookId);
-//     const p = new constructor();
+function WithTemplate(template: string, hookId: string) {
+  console.log("TEMPLATE FACTORY");
+  return function <T extends { new (...args: any[]): { name: string } }>(
+    originalConstructor: T
+  ) {
+    return class extends originalConstructor {
+      constructor(..._: any[]) {
+        super();
 
-//     if (hookEl) {
-//       hookEl.innerHTML = template;
-//       hookEl.querySelector("h1")!.textContent = p.name;
-//     }
-//   };
-// }
+        console.log("Rendering template");
+        const hookEl = document.getElementById(hookId);
 
-// // @Logger("LOGGING - PERSON")
-// @Logger("LOGGING")
-// @WithTemplate("<h1>My Person Object</h1>", "app")
-// class Person {
-//   name = "Sophie";
+        if (hookEl) {
+          hookEl.innerHTML = template;
+          hookEl.querySelector("h1")!.textContent = this.name;
+        }
+      }
+    };
+  };
+}
 
-//   constructor() {
-//     console.log("Creating person object...");
-//   }
-// }
+// @Logger("LOGGING - PERSON")
+@Logger("LOGGING")
+@WithTemplate("<h1>My Person Object</h1>", "app")
+class Person {
+  name = "Sophie";
 
-// const pers = new Person();
+  constructor() {
+    console.log("Creating person object...");
+  }
+}
 
-// console.log(pers);
+const pers = new Person();
+
+console.log(pers);
 
 //-----------
 
@@ -42,11 +49,37 @@ function Log(target: any, propertyName: string | Symbol) {
   console.log(target, propertyName);
 }
 
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor Decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method Decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter Decorator!");
+  console.log(target);
+  console.log(name);
+  console.log(position);
+}
+
 class Product {
   @Log
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -60,7 +93,38 @@ class Product {
     this._price = p;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     this._price * (1 + tax);
   }
 }
+
+const p1 = new Product("Book", 19);
+const p2 = new Product("Book 2", 29);
+
+function Autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    enumerable: false,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
+
+class Printer {
+  message = "This works!";
+
+  @Autobind
+  showMessage() {
+    console.log(this.message);
+  }
+}
+
+const p = new Printer();
+
+const button = document.querySelector("button")!;
+button.addEventListener("click", p.showMessage);
